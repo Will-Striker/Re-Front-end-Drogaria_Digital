0eb53be4b64676b2531b18a1ee37a8a289b5a72a
Will-Striker <79809859+Will-Striker@users.noreply.github.com>
2021-12-10 12:49:23 -0300

    Atualização do Adiministrador
    Foi acrescentado ao Adm a função do botão Dropdown, onde ele poderá observar as informações de sua conta e posteriormente caso queira, se deslogar.

---
+++ Novo-Projeto/Js/popover.js
@@ -0,0 +1,11 @@
+/*-------Inicio-JS-Popover------*/
+
+var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+  
+  /*-------Final-JS-Popover-------*/
+  
